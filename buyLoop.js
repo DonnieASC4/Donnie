c48594bfe64363ed53b9abf9ac88e90f9f5b9ecc
DonnieASC4 <dsparks18@deerfield.edu>
2017-07-13 10:32:30 -0400

    These are while loop practices especially whlieLoopPractice.js

--- buyLoop.js
+++ buyLoop.js
@@ -1,5 +1,33 @@
 var prompt = require('prompt-sync')();
-// 
-// get input from the user. 
-// 
-var n = prompt('How many more times? ');
+
+// Player start with $1000 dollars
+// They're buying items at a store before adventure.
+// They have to spend as much money as possible (Spend all money).
+
+
+/* Pseudocode: player's money equals 1000
+Use an array with Strings to store the items that the player bought
+until player runs out of money: Ask them what they want to buy. and Subtract price from their money. 
+*/
+
+var money = 1000;
+var items = [];
+
+// Condition is while player has money
+while(money > 0) {
+    console.log("You have $" + money + " left. A tent is $100. Used Yeezys are $500.");
+    var answer = prompt("Which item do you wnat to buy?");
+    if (answer === "tent") {
+        money-=100;
+        items.push("tent");
+        console.log("you bought a tent!");
+        } else if (answer == "Used Yeezys") {
+            money-=500;
+            items.push("Used Yeezys");
+            console.log("You bought Used Yeezys");
+            } else {
+                console.log("I didn't understand your answer. Try again!");
+            }
+
+}
+console.log("You are out of money")
---
+++ whileLoopPractice.js
@@ -0,0 +1,9 @@
+var j = 0;
+while(j < 10) {
+    console.log(j);
+    j++;
+}
+
+for(var i = 0; i < 10; i++) {
+    console.log(i);
+}
