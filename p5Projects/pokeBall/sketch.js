52cea90b06203e0d94fbac2460299b69fd09a8a7
DonnieASC4 <dsparks18@deerfield.edu>
2017-07-17 13:23:49 -0400

    These are the first p5 projects. pokemon-objects is a project

---
+++ p5Projects/pokeBall/sketch.js
@@ -0,0 +1,35 @@
+function setup() {
+    createCanvas(300,300);
+    background(210);
+    
+
+    //Regular PokeBall
+    // // Sets color of outline
+    // stroke("black");
+    // // Makes the pixel thickness 6 px thick. Thickness of shape.
+    // strokeWeight(6);
+
+    // // makes an arc
+    // //Width is the width of the canvas length is the length
+    // //QUARTER_PI = constant of .25*pi
+    // fill("white");
+    // arc(width/2, length/2 + 110, 200, 200, 0, PI, CHORD);
+    // //This comand is for the circle after it.
+    // fill("red");
+    
+    // // The angles are the 5th and 6th parameters it starts at the fifth parameter and goes colckwise to the 6th parameter
+    // arc(width/2, length/2 + 110, 200, 200, PI, 0, CHORD);
+    // // I assume CHORD is a constant
+
+    // ellipse(width/2, height/2 - 40, 20, 20,);
+    // fill("white");
+
+
+
+// Length isn't something. Only width and height are constants!
+    ellipse(width/2, height/2, 60, 60);
+    
+
+
+    
+}
--- pokemon-objects.js
+++ pokemon-objects.js
@@ -1,3 +1,5 @@
+var prompt = require('prompt-sync')();
+
 function PokeTemplate(name, att, def, hp, legend, type) {
     this.name = name;
     this.attack = att;
@@ -25,4 +27,69 @@ function AfterAttack() {
         console.log(instance.name + " is attacked. " +  instance.name + "'s HP was " + instance.hp + ".");
         instance.pokemonAttacked();
     }
+}
+
+// In a real program Pokemon Name would have to be referenced with a text file with all the pokemon's name
+// Pokemon Type would need to be referenced too.
+function AddPokemon() {
+    PokemonName = UserInput(true, false, false, false);
+    PokemonStats = UserInput(false, true, false, false);
+    PokemonIsLegend = UserInput(false, false, true, false);
+    PokemonType = UserInput(false, false, false, true);
+    console.log(PokemonName);
+   console.log(PokemonStats);
+console.log(PokemonIsLegend);
+    console.log(PokemonType);
+
+    //var arrayStats = PokemonStats.split(" ");
+
+}
+
+function UserInput(pokemon, number, boolean, type) {
+    if (pokemon == true) {
+       var pokemonName; 
+       // The do while loop works.
+       do { 
+           var pokemonName = prompt("Give the name of the pokemon. Only put in letters: ");
+       } while(HasANonLetter(pokemonName));
+        return pokemonName;
+    } else if (number == true) {
+        // Works
+        do { 
+            var Stat = prompt("Give the stats of the pokemon in this order Attack Defense HP. Don't put any letters or symbols and put spaces between each stat: ");
+        } while(HasANonNumber(Stat));
+        return Stat;
+    } else if (boolean == true) {
+        do { 
+       var bool = prompt("Enter true if the pokemon is a legendary. Otherwise enter false :");
+        } while(bool != "false" && bool != "true");
+        return bool;
+    } else {
+        // Do the array version later. This will only take in one Type.
+        do { 
+           var Type = prompt("Give the type of the pokemon. Only put in letters: ");
+        } while(HasANonLetter(Type));
+        return Type;
+    }
+}
+
+function HasANonLetter(pkmnName) {
+    for (var i = 0; i < pkmnName.length; i++) {
+        var char = pkmnName[i].charCodeAt()
+        if (!(char > 64 && char < 91) && !(char > 96 && char < 123)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function HasANonNumber(stat) {
+     for (var i = 0; i < stat.length; i++) {
+          var char = stat[i].charCodeAt();
+        // buggy
+        if (!(char > 47 && char < 58) && char != 32) {
+            return true;
+        }
+     }
+     return false;
 }
